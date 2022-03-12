0b7a3e52ec18c8aa3743c5153803584721cc6b60
Gean634N <gean.a.l.c@gmail.com>
2022-03-12 13:50:49 -0400

    setup inicial do canvas

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+  const loading = document.getElementById('loading');
+  loading.style.display = 'none';
+
+  const sprites = document.getElementById('sprites')
+  const canvas = document.getElementById('game-canvas');
+  const ctx = canvas.getContext('2d');
+})
